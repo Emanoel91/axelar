4e2b756b766ac48f2ff9326d9ac947f5c1a5e76f
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2026-07-02 02:42:27 +0330

    Update 4_💸ITS_Transfers.py

--- pages/4_💸ITS_Transfers.py
+++ pages/4_💸ITS_Transfers.py
@@ -129,62 +129,45 @@ df = load_data()
 
 st.sidebar.markdown(
     """
-<style>
+    <style>
+    .sidebar-footer {
+        position: fixed;
+        bottom: 20px;
+        width: 250px;
+        font-size: 13px;
+        color: gray;
+        margin-left: 5px; # -- MOVE LEFT
+        text-align: left;  
+    }
+    .sidebar-footer img {
+        width: 16px;
+        height: 16px;
+        vertical-align: middle;
+        border-radius: 50%;
+        margin-right: 5px;
+    }
+    .sidebar-footer a {
+        color: gray;
+        text-decoration: none;
+    }
+    </style>
 
-.sidebar-footer{
-position:fixed;
-bottom:20px;
-width:250px;
-font-size:13px;
-color:gray;
-margin-left:5px;
-text-align:left;
-}
-
-.sidebar-footer img{
-width:16px;
-height:16px;
-vertical-align:middle;
-border-radius:50%;
-margin-right:5px;
-}
-
-.sidebar-footer a{
-color:gray;
-text-decoration:none;
-}
-
-</style>
-
-<div class="sidebar-footer">
-
-<div>
-
-<a href="https://x.com/axelar" target="_blank">
-
-<img src="https://img.cryptorank.io/coins/axelar1663924228506.png">
-
-Powered by Axelar
-
-</a>
-
-</div>
-
-<div style="margin-top:5px;">
-
-<a href="https://x.com/0xeman_raz" target="_blank">
-
-<img src="https://pbs.twimg.com/profile_images/2060406047391559681/sA9zPNKM_400x400.jpg">
-
-Built by Eman Raz
-
-</a>
-
-</div>
-
-</div>
-""",
-unsafe_allow_html=True
+    <div class="sidebar-footer">
+        <div>
+            <a href="https://x.com/axelar" target="_blank">
+                <img src="https://img.cryptorank.io/coins/axelar1663924228506.png" alt="Axelar Logo">
+                Powered by Axelar
+            </a>
+        </div>
+        <div style="margin-top: 5px;">
+            <a href="https://x.com/0xeman_raz" target="_blank">
+                <img src="https://pbs.twimg.com/profile_images/2060406047391559681/sA9zPNKM_400x400.jpg" alt="Eman Raz">
+                Built by Eman Raz
+            </a>
+        </div>
+    </div>
+    """,
+    unsafe_allow_html=True
 )
 
 # =====================================================
